d96b1b1bca1ae6cfbae43c0fa14ab45e1d45deaf
Furqan Habib Siddiqui <furqanfreelance1@gmail.com>
2025-05-12 18:08:32 +0500

    Initial commit

--- android/app/build/intermediates/assets/debug/mergeDebugAssets/public/assets/differenceInCalendarMonths-CClJslyN.js
+++
@@ -1,1 +0,0 @@
-import{q as t}from"./index-BKzwkbhU.js";function e(e,n){const o=t(e),r=t(n);return 12*(o.getFullYear()-r.getFullYear())+(o.getMonth()-r.getMonth())}export{e as d};
---
+++ android/app/src/main/java/app/lovable/ccb1b3984ebf47e1ac451522f307f140/MainActivity.java
@@ -0,0 +1,5 @@
+package app.lovable.ccb1b3984ebf47e1ac451522f307f140;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
